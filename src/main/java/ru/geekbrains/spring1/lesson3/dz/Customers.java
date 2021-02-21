cf27c1ad9ce8859cae64d385806aa4afcc399325
IMBABOT1 <zlovred12@gmail.com>
2021-02-21 17:21:21 +0300

    postgres

--- src/main/java/ru/geekbrains/spring1/lesson3/dz/Customers.java
+++ src/main/java/ru/geekbrains/spring1/lesson3/dz/Customers.java
@@ -1,4 +1,12 @@
 package ru.geekbrains.spring1.lesson3.dz;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+
 public class Customers {
+    @Id
+    private Long ID;
+    private int name;
 }
--- src/main/java/ru/geekbrains/spring1/lesson3/dz/Items.java
+++ src/main/java/ru/geekbrains/spring1/lesson3/dz/Items.java
@@ -1,4 +1,15 @@
 package ru.geekbrains.spring1.lesson3.dz;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "items")
 public class Items {
+    @Id
+    private Long ID;
+    private String title;
+    private int cost;
+
 }
