ab1a0b009a71326e3751ed297f0ed7abafd1aed6
IMBABOT1 <zlovred12@gmail.com>
2021-02-24 15:14:13 +0300

    temp

--- src/main/java/ru/geekbrains/spring1/lesson3/dz/Customers.java
+++ src/main/java/ru/geekbrains/spring1/lesson3/dz/Customers.java
@@ -1,37 +1,10 @@
 package ru.geekbrains.spring1.lesson3.dz;
 
-import javax.persistence.*;
 import java.util.List;
 
-@Entity
 public class Customers {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long ID;
 
-    @Column(name = "name")
+    private int ID;
     private String name;
-
-    @OneToMany(mappedBy = "customers")
-    private List<Items> list;
-
-    public Customers(){
-    }
-
-    public void setID(Long ID) {
-        this.ID = ID;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getID() {
-        return ID;
-    }
-
-    public String getName() {
-        return name;
-    }
+    private List<Goods> list;
 }
