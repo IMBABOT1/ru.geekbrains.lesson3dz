8b28aeff6f6d5d23b0582d38e073da95e24d639f
IMBABOT1 <zlovred12@gmail.com>
2021-02-24 18:48:46 +0300

    temp

--- tables.sql
+++ tables.sql
@@ -1,16 +1,3 @@
-DROP TABLE IF EXISTS universities CASCADE;
-CREATE TABLE universities (id bigserial PRIMARY KEY, title VARCHAR(255));
-INSERT INTO universities (title) VALUES
-('DSTU'),
-('NPU');
-
-DROP TABLE IF EXISTS students CASCADE;
-CREATE TABLE students (id bigserial PRIMARY KEY, name VARCHAR(255), university_id bigint REFERENCES universities (id));
-INSERT INTO students (name, university_id) VALUES
-('Alexander', 1),
-('Bob', 2),
-('John', 1);
-
 DROP TABLE IF EXISTS customers CASCADE;
 CREATE TABLE customers (id bigserial PRIMARY KEY, name VARCHAR(255));
 INSERT INTO customers (name) VALUES
